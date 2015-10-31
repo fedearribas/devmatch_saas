427ff66cd69b9c23481f798b7c08bdce47243f4e
fedearribas <fede.arribas@gmail.com>
2015-10-31 15:39:44 +0000

    Added controller and nested routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,18 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {registrations: 'users/registrations'}
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
+  
+  resources :users do
+    resource :profile
+  end
+
   resources :contacts
   get '/about' => 'pages#about'
   root 'pages#home'
+  
+   # The priority is based upon order of creation: first created -> highest priority.
+  # See how all your routes lay out with "rake routes".
+  
+  
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
